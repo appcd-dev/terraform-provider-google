54585f7b390ad7b7a0713e521dc2126b6d665295
Sabith KS <sks@stackgen.com>
2025-03-03 17:01:21 -0800

    missed one

--- google/export.go
+++ google/export.go
@@ -1,6 +1,8 @@
 package google
 
 import (
+	"context"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-google/google/provider"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
@@ -14,4 +16,8 @@ func ConfigureBasePaths(c *Config) {
 
 func Provider() *schema.Provider {
 	return provider.Provider()
+}
+
+func (c *Config) LoadAndValidate(ctx context.Context) error {
+	return (*transport_tpg.Config)(c).LoadAndValidate(ctx)
 }
